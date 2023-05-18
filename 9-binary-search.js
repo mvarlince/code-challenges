3e3cc0859ac30f58f4b4fc6b49e65ec9aa1122c6
Varlince Meran <varlincemeran@gmail.com>
2023-05-17 21:53:03 -0400

    solved challenge 6 using binary search algorithm

---
+++ 9-binary-search.js
@@ -0,0 +1,29 @@
+/*
+Challenge: Binary Search
+
+Problem Statement:
+Given a sorted array of integers, implement the Binary Search algorithm to find the index of a specific target element. 
+If the target element is not found in the array, return -1.
+*/
+
+
+
+const example = [1, 2, 4, 5, 8, 9]
+
+function binarySearch(arr, target){
+    const midPoint = (arr.length / 2)
+
+    for (let i = 0; i < midPoint; i++){
+        if (arr[i] === target){
+            return i
+        }
+        for (let i = midPoint; i < arr.length; i++){
+            if (arr[i] === target){
+                return i
+            }
+        }
+    }
+    return -1
+}
+
+console.log(binarySearch(example, 1))
